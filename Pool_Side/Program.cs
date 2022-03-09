6b9e68546e3acb7b85192a1263e7e80e48e86a02
Botshelo Tlhabanyane <79863670+BotsheloT@users.noreply.github.com>
2022-03-09 08:53:29 +0200

    First Project Commit

---
+++ Pool_Side/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pool_Side
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
